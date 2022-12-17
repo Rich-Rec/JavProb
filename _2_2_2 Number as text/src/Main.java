9b60decaac8a3da0753f2361a7c3f0e7d434484d
Richard Recar <mrrrecar@icloud.com>
2022-12-17 14:41:57 -0500

    "2.2.2 Exercise 2"

--- _2_2_2 Number as text/src/Main.java
+++ _2_2_2 Number as text/src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        int n = 10;
+//        String inputToText = String.valueOf(n);
+//        System.out.println(inputToText.charAt(0));
+        String numberTest = numberAsText(87);
+        System.out.println(numberTest);
+
+    }
+
+    static public String numberAsText(final int n)
+        {
+            final int remainder = n % 10;
+            String inputToText = String.valueOf(n);
+            String valueAsText = "";
+            if (remainder == 0)
+                valueAsText = "ZERO";
+            if (remainder == 1)
+                valueAsText = "ONE";
+            if (remainder == 2)
+                valueAsText = "TWO";
+            if (remainder == 3)
+                valueAsText = "THREE";
+            if (remainder == 4)
+                valueAsText = "FOUR";
+            if (remainder == 5)
+                valueAsText = "FIVE";
+            if (remainder == 6)
+                valueAsText = "SIX";
+            if (remainder == 7)
+                valueAsText = "SEVEN";
+            if (remainder == 8)
+                valueAsText = "EIGHT";
+            if (remainder == 9)
+                valueAsText = "NINE";
+
+            return valueAsText;
     }
 }
