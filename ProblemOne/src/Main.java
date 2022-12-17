9ae662f1788e6a2dc511314461f6fdb7040b9f81
Richard Recar <mrrrecar@icloud.com>
2022-12-17 01:26:53 -0500

    "Problem One"

--- ProblemOne/src/Main.java
+++ ProblemOne/src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(calculation(3,4));
+
+        System.out.println(Arrays.toString(calcSumAndCountAllNumbersDivBy_2_Or_7(15)));
     }
     // Part A
     static public int calculation(int firstNumber, int secondNumber)
@@ -9,11 +13,11 @@ public class Main {
     }
 
     //Part B
-    public static int[] calcSumAndCountAllNumbersDivBy_2Or_7(int maxNumber)
+    public static int[] calcSumAndCountAllNumbersDivBy_2_Or_7(int maxNumber)
     {
         int [] returnValues = new int[2];
         int resultCount = 0, sumOfDivNumbers = 0;
-        for (int i = 0; i <= maxNumber; i++) {
+        for (int i = 1; i < maxNumber; i++) {
             if(i%2==0||i%7==0)
             {
                 sumOfDivNumbers+=i;
