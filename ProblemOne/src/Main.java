940323b88799fda21afe0c4eac4ccc2ec2d317e1
Richard Recar <mrrrecar@icloud.com>
2022-12-17 01:35:52 -0500

    "Problem One"

--- ProblemOne/src/Main.java
+++ ProblemOne/src/Main.java
@@ -2,16 +2,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(calculation(3,4));
-
-        System.out.println(Arrays.toString(calcSumAndCountAllNumbersDivBy_2_Or_7(15)));
+        System.out.println(calculation(3,4)); //1A
+        System.out.println(Arrays.toString(calcSumAndCountAllNumbersDivBy_2_Or_7(15)));//1B
+        System.out.println(isEven(11)); //1C
+        System.out.println(isOdd(11));
     }
     // Part A
     static public int calculation(int firstNumber, int secondNumber)
     {
         return ((firstNumber*secondNumber/2)%7);
     }
-
     //Part B
     public static int[] calcSumAndCountAllNumbersDivBy_2_Or_7(int maxNumber)
     {
@@ -27,5 +27,14 @@ public class Main {
         returnValues[0] = resultCount;
         returnValues[1] = sumOfDivNumbers;
         return returnValues;
+    }
+    //Part C
+    static public boolean isEven(int evenCheck)
+    {
+        return evenCheck % 2 == 0;
+    }
+    static public boolean isOdd(int oddCheck)
+    {
+        return oddCheck % 2 != 0;
     }
 }
