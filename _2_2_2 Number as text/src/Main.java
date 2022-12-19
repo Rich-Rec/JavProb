7e222fa2d70af0fe0752570499328ff86756666a
Richard Recar <mrrrecar@icloud.com>
2022-12-19 16:31:36 -0500

    "2.2.2 Complete"

--- _2_2_2 Number as text/src/Main.java
+++ _2_2_2 Number as text/src/Main.java
@@ -1,39 +1,36 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Map;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
 //        int n = 10;
 //        String inputToText = String.valueOf(n);
 //        System.out.println(inputToText.charAt(0));
-        String numberTest = numberAsText(87);
-        System.out.println(numberTest);
+        final int userInput = 43;
+       System.out.println(numberAsText(userInput));
 
     }
 
-    static public String numberAsText(final int n)
-        {
-            final int remainder = n % 10;
-            String inputToText = String.valueOf(n);
-            String valueAsText = "";
-            if (remainder == 0)
-                valueAsText = "ZERO";
-            if (remainder == 1)
-                valueAsText = "ONE";
-            if (remainder == 2)
-                valueAsText = "TWO";
-            if (remainder == 3)
-                valueAsText = "THREE";
-            if (remainder == 4)
-                valueAsText = "FOUR";
-            if (remainder == 5)
-                valueAsText = "FIVE";
-            if (remainder == 6)
-                valueAsText = "SIX";
-            if (remainder == 7)
-                valueAsText = "SEVEN";
-            if (remainder == 8)
-                valueAsText = "EIGHT";
-            if (remainder == 9)
-                valueAsText = "NINE";
+    static String numberAsText(final int n)
+    {
+        String value = "";
+        int remainingValue = n;
 
-            return valueAsText;
+        while(remainingValue > 0)
+        {
+            String remainderAsText = digitAsText(remainingValue%10);
+            value = remainderAsText + " " + value;
+
+            remainingValue /= 10;
+        }
+        return value.trim();
+    }
+
+    static Map<Integer, String> valueToTextMap = Map.of(0, "ZERO", 1, "ONE", 2, "TWO",3, "THREE", 4, "FOUR",5, "FIVE", 6, "SIX", 7, "SEVEN", 8, "EIGHT", 9, "NINE");
+
+    static String digitAsText(final int n)
+    {
+        return valueToTextMap.get(n%10);
     }
 }
