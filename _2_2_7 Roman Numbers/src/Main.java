a3921326647c5579450a4d2a9fa20adce2c765fb
Richard Recar <mrrrecar@icloud.com>
2023-01-12 16:16:13 -0500

    "_2_2_7a Roman Numbers --> Decimal Numbers"

--- _2_2_7 Roman Numbers/src/Main.java
+++ _2_2_7 Roman Numbers/src/Main.java
@@ -8,19 +8,23 @@ public class Main {
         System.out.println(numberResult);
     }
 
-    static int fromRomanNumber(String input)
-    {
+    static int fromRomanNumber(String input) {
         int numberFromRomanInput = 0;
 
         ArrayList<Integer> arrayOfConvertedValues = new ArrayList<>();
 
-        for (int i = 0; i <input.length(); i++) {
-            if(input.charAt(i)== 'X')
-            {
-                numberFromRomanInput = 10;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == 'X') {
+                numberFromRomanInput += 10;
             }
-        }
+            if (input.charAt(i) == 'V') {
+                numberFromRomanInput += 5;
+            }
+            if (input.charAt(i) == 'I') {
+                numberFromRomanInput += 1;
+            }
 
+        }
         return numberFromRomanInput;
     }
 }
